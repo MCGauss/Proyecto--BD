a480b61d84c57091cdcf9cfb2a0a9adb8604c4e6
DianaHang <dianavegat99@gmail.com>
2026-05-19 15:05:21 -0600

    Create MetodosPagos class

---
+++ src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/MetodosPagos.java
@@ -0,0 +1,30 @@
+package mx.unam.fes.acatlan.mac.proyectobd.backend.model;
+
+public class MetodosPagos {
+	//Atributos
+	protected int idMetodo;
+	protected String metodoPago;
+	
+	//Constructor
+	public MetodosPagos(int idMetodo, String metodoPago) {
+		this.idMetodo = idMetodo;
+		this.metodoPago = metodoPago;
+	}
+	
+	//Getters y setters
+	public int getIdMetodo() {
+		return idMetodo;
+	}
+	
+	public void setIdMetodo(int idMetodo) {
+		this.idMetodo = idMetodo;
+	}
+	
+	public String getMetodoPago() {
+		return metodoPago;
+	}
+	
+	public void setMetodoPago(String metodoPago) {
+		this.metodoPago = metodoPago;
+	}
+}
