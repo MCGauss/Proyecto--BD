0e21598a1b9dee7bd224d0453c80ff2e2fa1aff4
DianaHang <dianavegat99@gmail.com>
2026-05-20 12:26:09 -0600

    Update ENUM

--- src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/StatusPartido.java
+++ src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/StatusPartido.java
@@ -3,6 +3,6 @@ package mx.unam.fes.acatlan.mac.proyectobd.backend.model;
 public enum StatusPartido {
 	PROGRAMADO,
     EN_CURSO,
-    POSTPUESTO,
-    FINALIZADO
+    FINALIZADO,
+    POSPUESTO
 }
