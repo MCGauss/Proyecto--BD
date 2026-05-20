6875b2c4e63cc34b6b7ef30f4d7a231aafaa0ca5
DianaHang <dianavegat99@gmail.com>
2026-05-20 12:38:57 -0600

    Add 'CalcularPuntosLigaMX' class with its interface

---
+++ src/mx/unam/fes/acatlan/mac/proyectobd/backend/services/CalcularPuntosLigaMX.java
@@ -0,0 +1,14 @@
+package mx.unam.fes.acatlan.mac.proyectobd.backend.services;
+import mx.unam.fes.acatlan.mac.proyectobd.backend.interfaz.CalcularPuntos;
+import mx.unam.fes.acatlan.mac.proyectobd.backend.model.Partido;
+import mx.unam.fes.acatlan.mac.proyectobd.backend.model.Predicciones;
+
+public class CalcularPuntosLigaMX implements CalcularPuntos {
+
+	@Override
+	public int calcularPuntosObtenidos(Predicciones pred, Partido partido) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
