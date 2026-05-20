d648508aade34ab1d2a014bf282c5f12865c6b2a
DianaHang <dianavegat99@gmail.com>
2026-05-20 12:44:01 -0600

    Add DAO package

---
+++ src/mx/unam/fes/acatlan/mac/proyectobd/backend/DAO/EquiposDAO.java
@@ -0,0 +1,9 @@
+package mx.unam.fes.acatlan.mac.proyectobd.backend.DAO;
+
+import mx.unam.fes.acatlan.mac.proyectobd.backend.model.Equipos;
+/*import java.sql.Connection;
+import java.sql.PreparedStatement; */
+
+public class EquiposDAO {
+
+}
