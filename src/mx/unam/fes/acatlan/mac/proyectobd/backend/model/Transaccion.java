83283030d8d63144ba5ec6194806a25bc16fe68b
DianaHang <dianavegat99@gmail.com>
2026-05-20 12:23:06 -0600

    Update datatype from String to StatusTransaccion

--- src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/Transaccion.java
+++ src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/Transaccion.java
@@ -5,51 +5,50 @@ import java.time.LocalDateTime;
 public abstract class Transaccion {
 
 	//Atributos
-	protected int id;
-	protected double monto;
-	protected LocalDateTime fecha;
-	protected String status;
+	protected int idTrans;
+	protected double montoTrans;
+	protected LocalDateTime fechaTrans;
+	protected StatusTransaccion statusTrans; // PENDIENTE / APROBADO / RECHAZADO
 	
 	//Constructor
-	public Transaccion(int id, double monto, LocalDateTime fecha, String status) {
-		super();
-		this.id = id;
-		this.monto = monto;
-		this.fecha = fecha;
-		this.status = status;
+	public Transaccion(int idTrans, double montoTrans, LocalDateTime fechaTrans, StatusTransaccion statusTrans) {
+		this.idTrans = idTrans;
+		this.montoTrans = montoTrans;
+		this.fechaTrans = fechaTrans;
+		this.statusTrans= statusTrans;
 	}
 
 	//Getters y Setters
-	public int getId() {
-		return id;
+	public int getIdTrans() {
+		return idTrans;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setIdTrans(int idTrans) {
+		this.idTrans = idTrans;
 	}
 
-	public double getMonto() {
-		return monto;
+	public double getMontoTrans() {
+		return montoTrans;
 	}
 
-	public void setMonto(double monto) {
-		this.monto = monto;
+	public void setMontoTrans(double montoTrans) {
+		this.montoTrans = montoTrans;
 	}
 
-	public LocalDateTime getFecha() {
-		return fecha;
+	public LocalDateTime getFechaTrans() {
+		return fechaTrans;
 	}
 
-	public void setFecha(LocalDateTime fecha) {
-		this.fecha = fecha;
+	public void setFechaTrans(LocalDateTime fechaTrans) {
+		this.fechaTrans = fechaTrans;
 	}
 
-	public String getStatus() {
-		return status;
+	public StatusTransaccion getStatusTrans() {
+		return statusTrans;
 	}
 
-	public void setStatus(String status) {
-		this.status = status;
+	public void setStatusTrans(StatusTransaccion statusTrans) {
+		this.statusTrans = statusTrans;
 	} 
 	
 	
