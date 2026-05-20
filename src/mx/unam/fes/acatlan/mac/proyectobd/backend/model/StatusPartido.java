f1d0e777e63bc240877fdb515994f2bd7211397f
DianaHang <dianavegat99@gmail.com>
2026-05-20 12:24:45 -0600

    Update 'StatusPartido' from class to ENUM

--- src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/StatusPartido.java
+++ src/mx/unam/fes/acatlan/mac/proyectobd/backend/model/StatusPartido.java
@@ -1,28 +1,8 @@
 package mx.unam.fes.acatlan.mac.proyectobd.backend.model;
 
-public class StatusPartido {
-	// Atributos
-	protected int idStatusPartido;
-    protected String descripcionStatus;
-
-    public StatusPartido(int idStatusPartido, String descripcionStatus) {
-        this.idStatusPartido = idStatusPartido;
-        this.descripcionStatus = descripcionStatus;
-    }
-
-    // getters y setters
-    public int getIdStatusPartido() {
-    	return idStatusPartido; 
-    }
-    public void setIdStatusPartido(int idStatusPartido) {
-    	this.idStatusPartido = idStatusPartido; 
-    }
-
-    public String getDescripcionStatus() {
-    	return descripcionStatus;
-    }
-    public void setDescripcionStatus(String descripcionStatus) {
-    	this.descripcionStatus = descripcionStatus;
-    }
-
+public enum StatusPartido {
+	PROGRAMADO,
+    EN_CURSO,
+    POSTPUESTO,
+    FINALIZADO
 }
